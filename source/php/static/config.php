32f235645ddc65d28e8df5e4382af2cc69b76382
Mihai Gabriel Pascaru <30627752+mikisupor96@users.noreply.github.com>
2024-04-25 16:45:07 +0100

    Update config.php

--- source/php/static/config.php
+++ source/php/static/config.php
@@ -1,13 +1,13 @@
 <?php
 // https://www.geonames.org/
-$geonamesUsername = "mikisupor96";
+$geonamesUsername = "";
 // https://opencagedata.com/
-$geoCodeApiKey = "cf42cd0496c8439697af8e847cddb0ff";
+$geoCodeApiKey = "";
 // https://openweathermap.org/api
-$openWeatherApiKey = "48123a985b20a1619df8819a6d550170";
+$openWeatherApiKey = "";
 // https://newsapi.org/
-$newsApiKey = "1817bf312cb141f8a2d3d282b3c737c2";
+$newsApiKey = "";
 // https://api.windy.com/
-$windyApiKey = "RowCFQ4Bha61Zap9UTVXNQaWSA3QgSVG";
+$windyApiKey = "";
 
 
